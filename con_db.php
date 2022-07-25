0333100c989ef890d17dda673212cfbb6e674640
izzatidham99 <87406256+izzatidham99@users.noreply.github.com>
2022-07-26 01:58:52 +0800

    first commit

---
+++ con_db.php
@@ -0,0 +1,17 @@
+<?php
+define('db_user','root');
+define('db_passsword', null);
+define('db_host','localhost');
+define('db_name','lab7');
+
+$conn=mysqli_connect (db_host, db_user, db_passsword, db_name);
+$result=mysqli_query($conn,"select*from user");
+while($row=mysqli_fetch_array($result)){
+    foreach($row as $key=>$value){
+        echo "$key=$value <br>\n";
+    }
+}
+
+mysqli_free_result($result);
+mysqli_close($conn);
+?>
